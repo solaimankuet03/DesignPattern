e97f26429954d769f1ea640b3874d6035e4a9038
solaiman ali <solaimankuet03@gmail.com>
2020-11-12 10:23:57 +0600

    State design Pattern Real time example

---
+++ StateDesignPatternRealTimeExample/NoMoneyState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDesignPatternRealTimeExample
+{
+    public class NoMoneyState : IVendingMachineState
+    {
+        public void DispenseProduct()
+        {
+            Console.WriteLine("Vending Machine cannot dispense product because money is not inserted and product is not selected");
+        }
+
+        public void SelectProductAndInsertMoney(int amount, string productName)
+        {
+            Console.WriteLine(amount + "Rs has been inserted and " + productName + " has been selected");
+        }
+    }
+}
