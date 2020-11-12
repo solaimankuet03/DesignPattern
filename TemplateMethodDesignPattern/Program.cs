93aaa7ea4063ace71bd17bfefc5699f83105283c
solaiman ali <solaimankuet03@gmail.com>
2020-11-12 10:55:52 +0600

    TemplateMethodDesignPattern

---
+++ TemplateMethodDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a Concrete House\n");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            // call the template method
+            houseTemplate.BuildHouse();
+            Console.WriteLine();
+            Console.WriteLine("Build a Wooden House\n");
+            houseTemplate = new WoodenHouse();
+            // call the template method
+            houseTemplate.BuildHouse();
+            Console.Read();
+        }
+    }
+}
---
+++ TemplateMethodDesignPatternRealTimeExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodDesignPatternRealTimeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nescafe Coffee Preparation\n");
+            CoffeeTemplate coffee = new NescafeCoffee();
+            coffee.PrepareCoffee();
+            Console.WriteLine();
+
+            Console.WriteLine("Bru coffee preparation\n");
+            coffee = new BruCoffee();
+            coffee.PrepareCoffee();
+            Console.Read();
+        }
+    }
+}
