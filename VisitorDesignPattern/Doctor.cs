93f4010de536fecc0e98253ca6344a9c051fa65c
solaiman ali <solaimankuet03@gmail.com>
2020-11-18 10:32:40 +0600

    visitor design pattern

---
+++ VisitorDesignPattern/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorDesignPattern
+{
+    public class Doctor : IVisitor
+    {
+        public string Name { get; set; }
+
+        public Doctor(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+
+            Console.WriteLine("Doctor: " + this.Name + " did the health checkup of the child: " + kid.KidName);
+        }
+    }
+}
